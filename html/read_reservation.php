dfc199e64adec86fd96c3ea47f70489f9bea5510
FabCodeDev <fabricio3ad@gmail.com>
2023-10-08 12:36:31 -0500

    nueva correccion

--- html/read_reservation.php
+++ html/read_reservation.php
@@ -1,16 +1,75 @@
 <?php
 include("../admin/bd.php");
 
-$sql = "SELECT * FROM reserva_de_citas";
-$result = $conn->query($sql);
+// Función para obtener todas las reservas de citas desde la base de datos
+function getAllReservations($conn) {
+    $sql = "SELECT * FROM reserva_de_citas";
+    $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo "ID: " . $row["id_reserva"]. " - Cliente ID: " . $row["id_cliente"]. " - Fecha Reservada: " . $row["fecha_reservada"]. "<br>";
+    if ($result->num_rows > 0) {
+        return $result->fetch_all(MYSQLI_ASSOC);
+    } else {
+        return [];
     }
-} else {
-    echo "0 results";
 }
 
+// Obtener todas las reservas existentes
+$reservations = getAllReservations($conn);
+
+// Cerrar la conexión después de obtener los datos
 $conn->close();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD Reserva de Citas</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        table, th, td {
+            border: 1px solid black;
+        }
+
+        th, td {
+            padding: 10px;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+
+    <h2>Reservas de Citas</h2>
+
+    <!-- Botones para agregar, modificar y eliminar -->
+    <p>
+        <a href="new_reservation.php">Agregar</a> |
+        <a href="update_reservation.php">Modificar</a> |
+        <a href="delete_reservation.php">Eliminar</a>
+    </p>
+
+    <table>
+        <tr>
+            <th>ID Reserva</th>
+            <th>ID Cliente</th>
+            <th>Fecha Reservada</th>
+        </tr>
+
+        <?php foreach ($reservations as $reservation) : ?>
+            <tr>
+                <td><?php echo $reservation['id_reserva']; ?></td>
+                <td><?php echo $reservation['id_cliente']; ?></td>
+                <td><?php echo $reservation['fecha_reservada']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+
+</body>
+</html>
+
