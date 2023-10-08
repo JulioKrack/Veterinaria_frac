4c146a63d489a555f45062863e044c13f911e26c
FabCodeDev <fabricio3ad@gmail.com>
2023-10-08 00:27:01 -0500

    ga

--- html/new_reservation.php
+++ html/new_reservation.php
@@ -2,21 +2,23 @@
 
 include("../admin/bd.php");
 
-$id_cliente = $_POST['id_cliente'];
-$fecha_reservada = $_POST['fecha_reservada'];
-$asunto = $_POST['asunto'];
-$estado = $_POST['estado'];
-$id_administrador = $_POST['id_administrador'];
-$id_veterinario = $_POST['id_veterinario'];
+$id_cliente = isset($_POST['id_cliente']) ? $_POST['id_cliente'] : '';
+$fecha_reservada = isset($_POST['fecha_reservada']) ? $_POST['fecha_reservada'] : '';
+$asunto = isset($_POST['asunto']) ? $_POST['asunto'] : '';
+$estado = isset($_POST['estado']) ? $_POST['estado'] : '';
+$id_administrador = isset($_POST['id_administrador']) ? $_POST['id_administrador'] : '';
+$id_veterinario = isset($_POST['id_veterinario']) ? $_POST['id_veterinario'] : '';
 
 $sql = "INSERT INTO reserva_de_citas (id_cliente, fecha_reservada, asunto, estado, id_administrador, id_veterinario)
         VALUES ('$id_cliente', '$fecha_reservada', '$asunto', '$estado', '$id_administrador', '$id_veterinario')";
 
-if ($conn->query($sql) === TRUE) {
-    echo "Reservation created successfully";
+$result = $mysqli->query($sql);
+
+if (!$result) {
+    die('Error: ' . $mysqli->error);
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "Registro de cita exitoso";
 }
 
-$conn->close();
+$mysqli->close();
 ?>
